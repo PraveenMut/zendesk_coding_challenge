63572290ba3d044f77a0e14b92bcca4c0f2fda4f
praveen <contact@praveenmuthu.dev>
2019-06-05 00:19:57 +1000

    add testers for convenience methods and wrote the methods

--- model/application_server.rb
+++ model/application_server.rb
@@ -10,10 +10,10 @@ class RequestHandler
   @@authentication = "Basic cHJhdmVlbi5tdXRAZ21haWwuY29tOkphNHBUOUQzN0RkUFR5OHU4aEZV"
 
   def self.api_requester(get_all_tickets=nil, id="")
-    forbidden_response = "Authentication failed, check your credentials"
-    not_found_response = "API endpoint access failure"
-    server_error_response = "API unavailable at this time, check again later"
-    unknown_response = "Cannot access API"
+    # forbidden_response = "Authentication failed, check your credentials"
+    # not_found_response = "API endpoint access failure"
+    # server_error_response = "API unavailable at this time, check again later"
+    # unknown_response = "Cannot access API"
     if get_all_tickets
       http_response = HTTP.auth(@@authentication).get(@@req_url + "tickets.json")
     else
@@ -22,22 +22,28 @@ class RequestHandler
     if http_response.status != 200
       @@error_message = http_response.status # store the error in a variable for future reference
       if @@error_message == 401
-        return forbidden_response
+        return 401
       elsif @@error_message == 404
-        return not_found_response
+        return 404
       elsif @@error_message == 503
-        return server_error_response
+        return 503
       else
-        return unknown_response
+        return 400
       end
     end
     @@tickets = JSON.parse(http_response)
     return @@tickets
   end
 
-  def self.get_single_ticket(ticket_id)
-    api_requester(nil, ticket_id)
+  def self.retrieve_single_ticket(ticket_id)
+    ticket = api_requester(nil, ticket_id)
+    return ticket
+  end
+
+  def self.retrieve_all_tickets
+    api_requester(true)
+    return @@tickets
   end
 end
 
-p RequestHandler.api_requester(nil, 1)
+p RequestHandler.retrieve_all_tickets
--- tests/application_spec.rb
+++ tests/application_spec.rb
@@ -6,8 +6,7 @@ require './view/application_view.rb'
 RSpec.describe RequestHandler do
   describe '#api_requester' do
     it 'returns a 200 response for 1 request' do
-      expect(RequestHandler.api_requester(nil, 1)).not_to eq("Authentication failed, check your credentials" ||
-      "API endpoint access failure" || "Cannot access API" || "API unavailable at this time, check again later")
+      expect(RequestHandler.api_requester(nil, 1)).not_to eq(401 || 404 || 503 || 400)
     end
   end
 end
@@ -15,8 +14,7 @@ end
 RSpec.describe RequestHandler do
   describe '#api_requester' do
     it 'returns a 200 response for all ticket requests' do
-      expect(RequestHandler.api_requester(true)).not_to eq("Authentication failed, check your credentials" ||
-      "API endpoint access failure" || "Cannot access API" || "API unavailable at this time, check again later")
+      expect(RequestHandler.api_requester(true)).not_to eq(401 || 404 || 503 || 400)
     end
   end
 end
@@ -24,8 +22,36 @@ end
 RSpec.describe RequestHandler do
   describe '#api_requester' do
     it 'populates a response successfully for controller to work on' do
-      expect(RequestHandler.api_requester).not_to be_empty
+      expect(RequestHandler.api_requester(true)).not_to be_falsey
+      expect(RequestHandler.api_requester(nil, 1)).not_to be_falsey
     end
   end
 end
+
+RSpec.describe RequestHandler do
+  describe "#api_requester" do
+    it 'ensures that errorenous ticket id requests are handled' do
+      expect(RequestHandler.api_requester(nil, 5000)).to eq(404)
+    end
+  end
+end
+
+RSpec.describe RequestHandler do
+  describe "#retrieve_all_tickets" do
+    it "ensures that all tickets are retrieved safely without handling issues" do
+      expect(RequestHandler.retrieve_all_tickets).to include("tickets")
+    end
+  end
+end
+
+RSpec.describe RequestHandler do
+  describe "#retrieve_single_ticket" do
+    it "ensures that a single ticket is retrieved safely without handling issues" do
+      expect(RequestHandler.retrieve_single_ticket(1)).to include("ticket")
+      expect(RequestHandler.retrieve_single_ticket(100)).to include("ticket")
+      expect(RequestHandler.retrieve_single_ticket(5000)).to eq(404)
+    end
+  end
+end
+
 

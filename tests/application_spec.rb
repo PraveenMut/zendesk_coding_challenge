eb9e5965dacb4c85e69f7b06b64676efe41cf0d2
praveen <contact@praveenmuthu.dev>
2019-06-06 02:14:02 +1000

    created test to handle and display errors in view (ApplicationView)

--- tests/application_spec.rb
+++ tests/application_spec.rb
@@ -19,7 +19,7 @@ end
 
 RSpec.describe RequestHandler do
   describe '#api_requester' do
-    it 'returns a 200 response for all requests' do
+    it ': returns a 200 response for all requests' do
       expect(RequestHandler.api_requester(nil, 1)).not_to eq(401 || 404 || 503 || 400)
       expect(RequestHandler.api_requester(true)).not_to eq(401 || 404 || 503 || 400)
     end
@@ -28,7 +28,7 @@ end
 
 RSpec.describe RequestHandler do
   describe '#api_requester' do
-    it 'populates a response successfully for controller to work on' do
+    it ': populates a response successfully for controller to work on' do
       expect(RequestHandler.api_requester(true)).not_to be_falsey
       expect(RequestHandler.api_requester(nil, 1)).not_to be_falsey
       expect(RequestHandler.api_requester(true)["tickets"].length).to eq(100)
@@ -39,7 +39,7 @@ end
 
 RSpec.describe RequestHandler do
   describe "#api_requester" do
-    it 'ensures that errorenous ticket id requests are handled' do
+    it ': ensures that errorenous ticket id requests are handled' do
       expect(RequestHandler.api_requester(nil, 5000)).to eq(404)
     end
   end
@@ -47,7 +47,7 @@ end
 
 RSpec.describe RequestHandler do
   describe "#retrieve_all_tickets" do
-    it "ensures that all tickets are retrieved safely without handling issues" do
+    it ": ensures that all tickets are retrieved safely without handling issues" do
       expect(RequestHandler.retrieve_all_tickets).to include("tickets")
     end
   end
@@ -55,7 +55,7 @@ end
 
 RSpec.describe RequestHandler do
   describe "#retrieve_single_ticket" do
-    it "ensures that a single ticket is retrieved safely without handling issues" do
+    it ": ensures that a single ticket is retrieved safely without handling issues" do
       expect(RequestHandler.retrieve_single_ticket(1)).to include("ticket")
       expect(RequestHandler.retrieve_single_ticket(100)).to include("ticket")
       expect(RequestHandler.retrieve_single_ticket(5000)).to eq(404)
@@ -63,3 +63,12 @@ RSpec.describe RequestHandler do
   end
 end
 
+RSpec.describe ApplicationView do
+  describe "#error_handler" do
+    it ": handles errors correctly and displays them to screen" do
+      expect(ApplicationView.error_handler("Cannot access API", 400)).to match("Sorry! We have encountered an error: Cannot Access the API the error code is 400")
+      expect(ApplicationView.error_handler("Authentication failed, please check your credentials", 401)).to match("Sorry! We have encountered an error: Authentication Failed, please check your credentials")
+      expect(ApplicationController.error_handler(nil, nil)).to match("Sorry! An unknown error has occured. Please restart the program.")
+    end
+  end
+end

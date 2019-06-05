2f5052ffc2525e34be60db83536e6739414d8641
praveen <contact@praveenmuthu.dev>
2019-06-05 13:10:27 +1000

    add general structure to view

---
+++ controller/application_controller.rb
@@ -0,0 +1,2 @@
+require 'model/application_server.rb'
+require 'view/application_view.rb'
---
+++ view/application_view.rb
@@ -0,0 +1,22 @@
+class ApplicationView
+
+  def welcome_screen
+
+  end
+
+  def print_main_menu
+  end
+
+  def load_all_tickets
+  end
+
+  def load_single_ticket(ticket_id)
+  end
+
+  def show_all_tickets
+  end
+
+  def show_single_ticket
+  end
+
+end

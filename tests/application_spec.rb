c9e12e94ddc9a5b2dcef58f1c1f6fb34a0ae28d6
praveen <contact@praveenmuthu.dev>
2019-06-08 21:02:02 +1000

    commented out infinite loop methods (menu, show all), cannot be spec tested

--- tests/application_spec.rb
+++ tests/application_spec.rb
@@ -16,73 +16,73 @@ end
 class ApplicationViewTest < ApplicationView
 end
 
-# RSpec.describe RequestHandler do
-#   describe '#api_requester' do
-#     it ': returns a 200 response for all requests' do
-#       expect(RequestHandler.api_requester(nil, 1)).not_to eq(401 || 404 || 503 || 400)
-#       expect(RequestHandler.api_requester(true)).not_to eq(401 || 404 || 503 || 400)
-#     end
-#   end
-# end
+RSpec.describe RequestHandler do
+  describe '#api_requester' do
+    it ': returns a 200 response for all requests' do
+      expect(RequestHandler.api_requester(nil, 1)).not_to eq(401 || 404 || 503 || 400)
+      expect(RequestHandler.api_requester(true)).not_to eq(401 || 404 || 503 || 400)
+    end
+  end
+end
 
-# RSpec.describe RequestHandler do
-#   describe '#api_requester' do
-#     it ': populates a response successfully for controller to work on' do
-#       expect(RequestHandler.api_requester(true)).not_to be_falsey
-#       expect(RequestHandler.api_requester(nil, 1)).not_to be_falsey
-#       expect(RequestHandler.api_requester(true)["tickets"].length).to eq(100)
-#       expect(RequestHandler.api_requester(nil, 1)).to include("ticket")
-#     end
-#   end
-# end
+RSpec.describe RequestHandler do
+  describe '#api_requester' do
+    it ': populates a response successfully for controller to work on' do
+      expect(RequestHandler.api_requester(true)).not_to be_falsey
+      expect(RequestHandler.api_requester(nil, 1)).not_to be_falsey
+      expect(RequestHandler.api_requester(true)["tickets"].length).to eq(100)
+      expect(RequestHandler.api_requester(nil, 1)).to include("ticket")
+    end
+  end
+end
 
-# RSpec.describe RequestHandler do
-#   describe "#api_requester" do
-#     it ': ensures that errorenous ticket id requests are handled' do
-#       expect(RequestHandler.api_requester(nil, 5000)).to eq(404)
-#     end
-#   end
-# end
+RSpec.describe RequestHandler do
+  describe "#api_requester" do
+    it ': ensures that errorenous ticket id requests are handled' do
+      expect(RequestHandler.api_requester(nil, 5000)).to eq(404)
+    end
+  end
+end
 
-# RSpec.describe RequestHandler do
-#   describe "#retrieve_all_tickets" do
-#     it ": ensures that all tickets are retrieved safely without handling issues" do
-#       expect(RequestHandler.retrieve_all_tickets).to include("tickets")
-#     end
-#   end
-# end
+RSpec.describe RequestHandler do
+  describe "#retrieve_all_tickets" do
+    it ": ensures that all tickets are retrieved safely without handling issues" do
+      expect(RequestHandler.retrieve_all_tickets).to include("tickets")
+    end
+  end
+end
 
-# RSpec.describe RequestHandler do
-#   describe "#retrieve_single_ticket" do
-#     it ": ensures that a single ticket is retrieved safely without handling issues" do
-#       expect(RequestHandler.retrieve_single_ticket(1)).to include("ticket")
-#       expect(RequestHandler.retrieve_single_ticket(100)).to include("ticket")
-#       expect(RequestHandler.retrieve_single_ticket(5000)).to eq(404)
-#     end
-#   end
-# end
+RSpec.describe RequestHandler do
+  describe "#retrieve_single_ticket" do
+    it ": ensures that a single ticket is retrieved safely without handling issues" do
+      expect(RequestHandler.retrieve_single_ticket(1)).to include("ticket")
+      expect(RequestHandler.retrieve_single_ticket(100)).to include("ticket")
+      expect(RequestHandler.retrieve_single_ticket(5000)).to eq(404)
+    end
+  end
+end
 
-# RSpec.describe ApplicationView do
-#   describe "#error_handler" do
-#     it ": handles errors correctly" do
-#       expect(ApplicationView.error_handler("Cannot access API", 400)).to eq(1)
-#       expect(ApplicationView.error_handler("Authentication failed, please check your credentials", 401)).to eq(1)
-#       expect(ApplicationView.error_handler(nil, nil)).to eq(-1)
-#     end
-#   end
-# end
+RSpec.describe ApplicationView do
+  describe "#error_handler" do
+    it ": handles errors correctly" do
+      expect(ApplicationView.error_handler("Cannot access API", 400)).to eq(1)
+      expect(ApplicationView.error_handler("Authentication failed, please check your credentials", 401)).to eq(1)
+      expect(ApplicationView.error_handler(nil, nil)).to eq(-1)
+    end
+  end
+end
 
-# RSpec.describe ApplicationView do
-#   describe "#error_handler" do
-#     context ": displays errors correctly to screen" do
-#       specify { expect { ApplicationView.error_handler("Cannot Access API", 400) }.to output.to_stdout }
-#       specify { expect { ApplicationView.error_handler("Bad Request. Cannot Access API", 400) }.to output(/Access API/).to_stdout }
-#       specify { expect { ApplicationView.error_handler("Authentication failed, please check your credentials", 401) }.to output(/(Authentication)/).to_stdout }
-#       specify { expect { ApplicationView.error_handler("API unavailable at this time, check again later", 503)}.to output(/(unavailable)/).to_stdout }
-#       specify { expect { ApplicationView.error_handler(nil, nil)}.to output(/unknown/).to_stdout }
-#     end
-#   end
-# end
+RSpec.describe ApplicationView do
+  describe "#error_handler" do
+    context ": displays errors correctly to screen" do
+      specify { expect { ApplicationView.error_handler("Cannot Access API", 400) }.to output.to_stdout }
+      specify { expect { ApplicationView.error_handler("Bad Request. Cannot Access API", 400) }.to output(/Access API/).to_stdout }
+      specify { expect { ApplicationView.error_handler("Authentication failed, please check your credentials", 401) }.to output(/(Authentication)/).to_stdout }
+      specify { expect { ApplicationView.error_handler("API unavailable at this time, check again later", 503)}.to output(/(unavailable)/).to_stdout }
+      specify { expect { ApplicationView.error_handler(nil, nil)}.to output(/unknown/).to_stdout }
+    end
+  end
+end
 
 # RSpec.describe ApplicationController do
 #   describe '#menu_control' do
@@ -193,22 +193,7 @@ RSpec.describe ApplicationController do
     # end  
 
     context 'shows the first paginated list when run' do
-      specify { expect{ ApplicationController.show_all }.to_output(/^.*(Sample|Ticket).*$/) }
-    end
-
-    context 'shows the second paginated list when run' do
-    end
-
-    context 'shows an error when page does not exist (over last page)' do
-    end
-
-    context 'shows an error when page does not exist (before first page)' do
-    end
-
-    context 'shows the main menu when user inputs "m"' do
-    end
-
-    context 'quits program when the user inputs "q"' do
+      specify { expect { ApplicationController.show_all }.to_output(/^.*(Sample|Ticket).*$/) }
     end
   end
 end
